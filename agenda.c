340c58160a53e75761b6a6f0492f47c39d6e3d08
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-04-05 12:05:34 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -10,13 +10,13 @@ typedef struct{
 
 void* adiciona(void *pBuffer,int *cp, int *c, float *a){
     pessoa *novo;
-    cp ++;
+    *cp+=1;
     pBuffer = realloc ( pBuffer, sizeof(int)*2+sizeof(float)+(*cp)*(sizeof(pessoa)	)	);
     c = (int*)pBuffer;
     cp =(int*)c + 1;
     a = (float*)cp + 1;
-	
-    novo = (pessoa*)(a + ((int)(sizeof(pessoa)) * ( *cp ) ));
+	*c = (sizeof(pessoa)) * ( *cp );
+    novo = (pessoa*)(a + (*c));
 
     printf("nome:");
     scanf("%s",(*novo).nome);
@@ -28,14 +28,16 @@ void* adiciona(void *pBuffer,int *cp, int *c, float *a){
     return pBuffer;
     }
 
-void lista(void *pBuffer, int *c, pessoa *init,int *cp, pessoa *fw){
+void lista(void *pBuffer, int *c,int *cp, pessoa *fw){
 
-    fw= init;
-    for(*c=0 ; *c!=*cp ; *c++ ){
+    fw = (pessoa*)pBuffer;
+    fw+=(sizeof(int))*2+sizeof(float);
+
+    for(*c=0 ; *c!=*cp ; (*c)++ ){
 	printf("nome:%s\n",(*fw).nome);
-	printf("idade:%d\n",&(*fw).idade);
-	printf("matricula:%d\n",&(*fw).mat);
-	
+	printf("idade:%d\n",(*fw).idade);
+	printf("matricula:%d\n",(*fw).mat);
+
 	fw++;
     }
 
@@ -45,28 +47,35 @@ void lista(void *pBuffer, int *c, pessoa *init,int *cp, pessoa *fw){
 
 int main(){
     void *pBuffer;
-    int *c,*cp=0;
+    int *c,*cp;
+
     float *a;
-    pessoa *init,*fw;
+    pessoa *fw;
     pBuffer = malloc((sizeof(int)*2)+sizeof(float));
 	c=pBuffer;
 	cp =(int*)c + 1;
+    *cp = 0;
     a = (float*)cp + 1;
-	
-    printf("(1)adicionar a agenda:\n(2)retirar da agenda:\n(3)procurar:\n(4)lista:\n");
-    scanf("%d",&*c);
-  
-  
+
+
   	do{
-  	
-    switch(*c){
-	
-    case 1:
-	adiciona(pBuffer,cp,c,a);
-	break;
-    case 4:lista(pBuffer,c,init,cp,fw);
-    break;
-	}
-    return 0;
-}while(*c!=5);
+  	    printf("\n\n(1)adicionar a agenda:\n(2)retirar da agenda:\n(3)procurar:\n(4)lista:\n");
+        scanf("%d",&*c);
+
+        switch(*c){
+
+          case 1:
+                pBuffer= adiciona(pBuffer,cp,c,a);
+            break;
+            case 4:
+                lista(pBuffer,c,cp,fw);
+            break;
+
+            default:
+            return 0;
+            break;
+	        }
+
+    }while(*c!=5);
+
 }
