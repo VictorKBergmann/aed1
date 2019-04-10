990410524ce073e1a925099c4543d5f74609fb68
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-04-10 19:18:46 -0300

    adicionei o remover

--- agenda.c
+++ agenda.c
@@ -1,56 +1,36 @@
+// Nao se preocupem com isso
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define RESET   "\x1b[0m"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
 #include <string.h>
 
-typedef struct{
-    char nome[20];
-    int idade, mat,id;
-    }pessoa ;
+// Define uteis
+#define OFFSET 97
+#define ALFABETO 26
+#define true 1
+#define false 0
+#define bool int
+#define INVALIDO -1
+#define DEFAULT "Pais desconhecido\0"
 
+// Estruturas de dados
+struct Celula {
+    char * pais;
+    char * frase;
+};
 
-int *c,*cp;
-void *pBuffer;
-pessoa *fw;
+struct Dicionario {
+    struct Celula letras[ALFABETO];
+};
 
+typedef struct Celula Celula;
+typedef struct Dicionario Dicionario;
 
-void adiciona(){
-    pessoa *novo;
-    *cp+=1;
-    pBuffer = realloc ( pBuffer, sizeof(int)*2+(*cp + 1)*(sizeof(pessoa)	)	);
-    c = (int*)pBuffer;
-    cp =(int*)c + 1;
-    fw = (pessoa*)cp + 1;
-    novo = fw + ( *cp - 1 );
-
-	novo->id = *cp - 1;  
-    printf("nome:");
-    scanf(" %s",novo->nome);
-    printf("idade:");
-    scanf("%d",&novo->idade);
-    printf("matricula:");
-    scanf("%d",&novo->mat);
-
-    }
-    
-void remove{
-	pessoa *novo;
-	novo = fw + ( *cp - 1 );
-	printf("nome:");
-    scanf(" %s",novo->nome); 
-    
-    for(*c=0;*c != *cp; *c++){	
-		if (strcmp( (fw+(*c))->nome,novo->nome ) == 0 ) {
-			
-		}
-		
-	}
-}
-
-}
-    
-
-/*int pesquisar(Dicionario * dicionario, char * frase) {
+// Pesquisar : retorna true se a frase esta no dicionario, false caso contrario
+int pesquisar(Dicionario * dicionario, char * frase) {
     for (int i = 0; i < ALFABETO; i++) {
         if (dicionario->letras[i].frase != NULL) {
             if (strcmp(dicionario->letras[i].frase, frase) == 0) {
@@ -59,77 +39,142 @@ void remove{
         }
     }
     return INVALIDO;
-}*/
-
-void pesquisa(){
-	pessoa *novo;
-	novo = fw + ( *cp - 1 );
-	printf("nome:");
-    scanf(" %s",novo->nome);
-	
-	for(*c=0;*c != *cp; *c++){	
-		if (strcmp( (fw+(*c))->nome,novo->nome ) == 0 ) {
-			printf("\nid:%d",(fw+(*c))->id);
-			printf("\nnome:%s\n",(fw+(*c))->nome);
-			printf("idade:%d\n",(fw+(*c))->idade);
-			printf("matricula:%d\n\n",(fw+(*c))->mat);
-		}
-		
-	}
 }
 
+// Inserir : insere par pais - frase no dicionario caso a primeira letra do pais esteja livre e retorna true, false caso nao insira
+bool inserir(Dicionario * dicionario, char * pais, char * frase) {
+    int t;
+    for(int i=0; i != ALFABETO; i++){
+    	if( pais[0] == OFFSET + i && dicionario->letras[i].pais == NULL ){
 
-void lista(){
+    		t = strlen(pais);
+    		dicionario->letras[i].pais = (char*)malloc(sizeof(char)*t);
+    		strcpy( dicionario->letras[i].pais , pais );
+            /*t--;
+            for(;t!=-1 ; t-- ){
+                dicionario->letras[i].pais[t] = pais[t];
+            }*/
 
-    for(*c=0 ; *c!=*cp ; (*c)++ ){
-    printf("\nid:%d",(fw+(*c))->id);
-	printf("\nnome:%s\n",(fw+(*c))->nome);
-	printf("idade:%d\n",(fw+(*c))->idade);
-	printf("matricula:%d\n\n",(fw+(*c))->mat);
+            t = strlen(frase);
+            dicionario->letras[i].frase = (char*)malloc(sizeof(char)*t);
+
+            /*for(;t!=-1 ; t-- ){
+                dicionario->letras[i].frase[t] = frase[t];
+            }*/
+    		strcpy( dicionario->letras[i].frase, frase );
+   			return true;
+   		}
+   	}
+    return false;
+}
+
+// Identificar : retorna a a traducao de "Feliz Natal!" no pais passado como parametro caso esteja no dicionario, retorna frase DEFAULT caso contrario
+char * identificar(Dicionario * dicionario, char * frase) {
+
+    for (int i = 0; i < ALFABETO; i++) {
+        if(dicionario->letras[i].frase!= NULL ){
+               if (strcmp(dicionario->letras[i].frase, frase) == 0)  {
+
+                return (dicionario->letras[i].pais);
+            }
+        }
     }
 
+    return DEFAULT;
 }
 
+//função que liberara a memoria
+void fre(Dicionario *d){
+    for(int i=0; i != ALFABETO; i++){
+            free(d->letras[i].frase);
+            free(d->letras[i].pais);
+    }
+    free(d);
+}
+
+// Inicializa dicionario
+Dicionario * initDicionario() {
+    Dicionario * d = (Dicionario *) malloc(sizeof(Dicionario));
+
+    for (int i = 0; i < ALFABETO; i++) {
+        d->letras[i].pais = NULL;
+    }
+
+    printf("Inserindo brasil\n");
+    inserir(d, "brasil\0", "Feliz Natal!\0");
+
+    printf("Inserindo alemanha\n");
+    inserir(d, "alemanha", "Frohliche Weihnachten!\0");
+
+    printf("Inserindo coreia\n");
+    inserir(d, "coreia", "Chuk Sung Tan!\0");
+
+    printf("Inserindo grecia\n");
+    inserir(d, "grecia", "Kala Christougena!\0");
+
+    printf("Inserindo estados-unidos\n");
+    inserir(d, "estados-unidos", "Merry Christmas!\0");
+
+    printf("Inserindo suecia\n");
+    inserir(d, "suecia", "God Jul!");
+
+    printf("Inserindo turquia\n");
+    inserir(d, "turquia", "Mutlu Noeller\0");
+
+    printf("Inserindo mexico\n");
+    inserir(d, "mexico", "Feliz Navidad!\0");
+
+    printf("Inserindo italia\n");
+    inserir(d, "italia", "Buon Natale!\0");
+
+    printf("Inserindo japao\n");
+    inserir(d, "japao", "Merii Kurisumasu!\0");
+
+    return d;
+}
+
+int main() {
+    printf("---Inicializacao---\n");
+    Dicionario * d = initDicionario();
+    char * resultado;
+
+    printf("\n---Testes de Validacao---\n");
+    printf("1. Pesquisa por traducao existente");
+    if (pesquisar(d, "Feliz Natal!\0") != INVALIDO) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("2. Pesquisa por traducao nao existente");
+    if (pesquisar(d, "Nollaig Shona Dhuit!\0") == INVALIDO) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("3. Saudacao valida");
+    if (strcmp(identificar(d, "Merii Kurisumasu!\0"), "japao") == 0) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("4. Saudacao invalida");
+    if (strcmp(identificar(d, "Nollaig Shona Dhuit!\0"), DEFAULT) == 0) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
 
 
-
-int main(){
-
-    pBuffer = malloc((sizeof(int)*2));
-	c=(int*)pBuffer;
-	cp =(int*)c + 1;
-	fw = (pessoa*)cp + 1;
-    *cp = 0;
+    //adicionada a função que liberara a memoria
+    fre(d);
 
 
-  	do{
-  	    printf("\n\n(1)adicionar a agenda:\n(2)retirar da agenda:\n(3)procurar:\n(4)lista:\n(5)sair:");
-        scanf("%d",&*c);
-
-        switch(*c){
-
-        	case 1:
-                adiciona();
-            break;
-        	
-        	case 2:
-        		remove();
-			break;       		
-        		
-			case 3:
-                pesquisa();
-            break;
-			
-			case 4:
-                lista();
-            break;
-			
-            default:
-            	free(pBufeer);
-                return 0;
-			break;
-	        }
-
-    }while(*c>0 && *c < 5);
-
+    return 0;
 }
