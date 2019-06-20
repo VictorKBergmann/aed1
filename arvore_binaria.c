24b7b662336947540dcc801e570f63dae2ea1334
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-06-20 17:58:06 -0300

    Update arvore_binaria.c

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -1,69 +1,103 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 typedef struct No *Apontador;
 typedef struct Registro{
     int data;
 }Registro;
 typedef struct No{
     Registro Reg;
-	Apontador rigth,left;
+    Apontador rigth,left,parent;
+    
 }No;
 
-int conta(No*node){
+int conta(No**node){
     int a=1;
-	if(node->left != NULL)a +=conta(node->left);
-	if(node->rigth != NULL)a +=conta(node->rigth);
+    if(node->left != NULL)a +=conta(node->left);
+    if(node->rigth != NULL)a +=conta(node->rigth);
 
-	return a;
+    return a;
 }
-int contaNos(No*node){
+int contaNos(No**node){
     int a=0;
-	if(node->left != NULL)a +=conta(node->left);
-	if(node->rigth != NULL)a +=conta(node->rigth);
+    if(node->left != NULL)a +=contaNos(node->left);
+    if(node->rigth != NULL)a +=contaNos(node->rigth);
 
-	return a;
+    return a;
 }
-int contaFolhas(No*node){
+int contaFolhas(No**node){
     int a=0;
-	if(node->left == NULL)a +=conta(node->left);
-	else a++;
-	if(node->rigth == NULL)a +=conta(node->rigth);
+    if(node->left == NULL)a +=contaFolhas(node->left);
+    else a++;
+    if(node->rigth == NULL)a +=contaFolhas(node->rigth);
     else a++;
 
-	return a;
+    return a;
 }
-int altura(No*node){
+int altura(No**node){
     int a=1,b=1;
-	if(node->left != NULL)a +=conta(node->left);
-	if(node->rigth != NULL)b +=conta(node->rigth);
+    if(node->left != NULL)a +=altura(node->left);
+    if(node->rigth != NULL)b +=altura(node->rigth);
 
     if(a>b)return a;
     else return b;
 
 }
-Apontador* procura(No*node){
-    if(node->Reg.data%2==1) return node;
+No** procuraImpar(No**node){
+    if(node->Reg.data%2==1) return *node;
 
     if(node->left != NULL)
-        if(procura(node->left)!=NULL)return node;
-	if(node->rigth != NULL)
-        if(procura(node->rigth)!=NULL)return node;
-	return NULL;
+        if(procura(node->left)!=NULL)return *node;
+    if(node->rigth != NULL)
+        if(procura(node->rigth)!=NULL)return *node;
+    return NULL;
 }
-void inicializa(No*node){
-    node->Reg.data +=10;
-    node->left->Reg.data -=1;
-    node->rigth->Reg.data +=1;
-    inicializa(node->left);
-    inicializa(node->rigth);
+void insere(No**node,int num){
+    if(*node==NULL)
+        *node=(No*)malloc(sizeof(No));
+        (*node)->Reg->data = num;
+        (*node)->rigth = NULL;
+        (*node)->left = NULL;
+        return; 
+    else if(*node->Reg->data >num)
+            insere(&(*node)->left,num);
+            return;
+        else
+            insere(&(*node)->rigth,num);
+            return;
+}
+void inicializa(No**node){
+    insere(*node,10);
+    insere(*node,7);
+    insere(*node,3);
+    insere(*node,4);
+    insere(*node,11);
+    insere(*node,1);
 
 }
 
+No* parent(No**node){
+    return node->parent;
+}
+
+No* grandparent(No**node){
+    return node->parent->parent;;
+}
+
+
+
+void printa(No** node,int i){
+    int t=i;
+    for(;t!=0;t--)
+        printf("    ");
+    printf("%d",node->Reg.data);
+    if(node->left != NULL)printa(node->left,i+1);
+    if(node->rigth!= NULL)printa(node->rigth,i+1);
+
+    return;
+}
 
 int main(){
     Apontador* raiz;
     inicializa(raiz);
     }
-
